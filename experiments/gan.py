301266a42de52279b89f0d7b7d0d5c96f4b84507
mbbrodie <mikebbrodie@gmail.com>
2018-01-18 20:18:20 -0700

    add vanilla gan

---
+++ experiments/gan.py
@@ -0,0 +1,105 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+#see https://raw.githubusercontent.com/caogang/wgan-gp/master/gan_mnist.py
+class C(nn.Module):
+    def __init__(self):
+        super(C, self).__init__()
+
+        main = nn.Sequential(
+            nn.Conv2d(1, DIM, 5, stride=2, padding=2),
+            nn.ReLU(True),
+            nn.Conv2d(DIM, 2*DIM, 5, stride=2, padding=2),
+            nn.ReLU(True),
+            nn.Conv2d(2*DIM, 4*DIM, 5, stride=2, padding=2),
+            nn.ReLU(True),
+        )
+        self.main = main
+        self.output = nn.Linear(4*4*4*DIM, 1)
+
+    def forward(self, input):
+        input = input.view(-1, 1, 28, 28)
+        out = self.main(input)
+        out = out.view(-1, 4*4*4*DIM)
+        out = self.output(out)
+        return out.view(-1)
+
+class G(nn.Module):
+    def __init__(self):
+        super(G, self).__init__()
+
+        preprocess = nn.Sequential(
+            nn.Linear(128, 4*4*4*DIM),
+            nn.ReLU(True),
+        )
+        block1 = nn.Sequential(
+            nn.ConvTranspose2d(4*DIM, 2*DIM, 5),
+            nn.ReLU(True),
+        )
+        block2 = nn.Sequential(
+            nn.ConvTranspose2d(2*DIM, DIM, 5),
+            nn.ReLU(True),
+        )
+        deconv_out = nn.ConvTranspose2d(DIM, 1, 8, stride=2)
+
+        self.block1 = block1
+        self.block2 = block2
+        self.deconv_out = deconv_out
+        self.preprocess = preprocess
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input):
+        output = self.preprocess(input)
+        output = output.view(-1, 4*DIM, 4, 4)
+        output = self.block1(output)
+        output = output[:, :, :7, :7]
+        output = self.block2(output)
+        output = self.deconv_out(output)
+        output = self.sigmoid(output)
+        return output.view(-1, OUTPUT_DIM)
+
+class EnsGAN:
+    def __init__(self,n=4):
+        gs = []
+        for i in xrange(n):
+            gs.append(G()) 
+        c = C() 
+
+    def train(method='wgan-wp'):
+        pass   
+
+
+
+ 
+'''
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+'''
